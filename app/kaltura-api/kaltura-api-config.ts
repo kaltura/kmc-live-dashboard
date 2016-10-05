2743830d0a05e16af60eb0d55ca3fe0fa4f19488
ainvoner <shai.ainvoner@IL-SHAIAM-RND-3.local>
2016-10-05 17:24:56 +0300

    adding KMC menu bar

--- app/kaltura-api/kaltura-api-config.ts
+++ app/kaltura-api/kaltura-api-config.ts
@@ -3,7 +3,9 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class KalturaAPIConfig {
 
+    //noinspection TypeScriptUnresolvedVariable
     ks : string = window.parent.kmc.vars.ks || "djJ8MTgwMjM4MXyxrZt-jJdDm4lPrbqcRw9EkkW58t4O2i2J3hA8CDFk6-j0dtgvanzHd27lKeuyJEe0WYZswYzkuvRvKq98TxXc6zz7wFt6z_D5Gls4K43urat_XnxjpNFCE8ixbmKevQizNff6wHsLkGLipIgpZrdc";
+    //noinspection TypeScriptUnresolvedVariable
     apiUrl : string = (window.parent.kmc.vars.api_url || "https://www.kaltura.com")  + "/api_v3/index.php";
     apiVersion : string;
 
