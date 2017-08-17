7f8f5aa3d4c07e170c42fa63ca85f5492fd63d13
gadyaari <gad.yaari@kaltura.com>
2017-08-17 12:14:54 +0300

    Window sizes refactoring - adjust to kmc iframe window

--- Live-dashboard/src/environments/environment.prod.ts
+++ Live-dashboard/src/environments/environment.prod.ts
@@ -1,5 +1,8 @@
 export const environment = {
   production: true,
+  "bootstrap": {
+    "service_url_extension": "/api_v3/index.php"
+  },
   "liveEntryService": {
     "streamStatusIntervalTimeInMs": 5000,
     "streamHealthIntervalTimeInMs": 10000
