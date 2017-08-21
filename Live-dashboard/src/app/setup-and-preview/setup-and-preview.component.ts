df7bbe2e9fdf1e20eb5536bd326fa590d7801de4
gadyaari <gad.yaari@kaltura.com>
2017-08-21 19:26:14 +0300

    Refactor application load-up: 1) Support load of "stream is live" screen immediately when application starts 2) Display error message if not all necessary parameters are available

--- Live-dashboard/src/app/setup-and-preview/setup-and-preview.component.ts
+++ Live-dashboard/src/app/setup-and-preview/setup-and-preview.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { LiveEntryService, ApplicationStatus, LiveEntryDynamicStreamInfo } from "../live-entry.service";
+import { LiveEntryService, ApplicationStatus, LiveEntryDynamicStreamInfo, LoadingStatus } from "../live-entry.service";
+import { TranslateService } from "ng2-translate";
+import { AreaBlockerMessage } from "@kaltura-ng/kaltura-ui";
 import { environment } from "../../environments/environment";
 
 import 'rxjs/Rx';
@@ -13,17 +15,17 @@ import 'rxjs/Rx';
 export class SetupAndPreviewComponent implements OnInit {
 
   public _applicationStatus: ApplicationStatus;
+  public _sectionBlockerMessage: AreaBlockerMessage;
   public _dynamicInformation: LiveEntryDynamicStreamInfo;
   public _learnMoreLink = environment.externalLinks.LEARN_MORE;
 
-  constructor(public _liveEntryService : LiveEntryService) {
-    this._applicationStatus = { status: 'initial' };
+  constructor(public _liveEntryService : LiveEntryService, private _translate: TranslateService) {
     this._dynamicInformation = { streamStatus: 'Offline' };
   }
 
   ngOnInit() {
     this._liveEntryService.applicationStatus$.subscribe(response => {
-       if(response) {
+       if (response) {
          this._applicationStatus = response;
        }
     });
@@ -32,6 +34,22 @@ export class SetupAndPreviewComponent implements OnInit {
         this._dynamicInformation.streamStatus = response.streamStatus;
       }
     });
+  }
+
+  public _applicationLoaded(): boolean {
+    if (this._applicationStatus.liveEntry === LoadingStatus.succeeded &&
+        this._applicationStatus.streamStatus === LoadingStatus.succeeded &&
+        this._applicationStatus.streamHealth === LoadingStatus.succeeded) {
+      return false;
+    }
+    else if (this._applicationStatus.liveEntry === LoadingStatus.failed) {
+      this._sectionBlockerMessage = new AreaBlockerMessage({
+        message: this._translate.instant(environment.loadingError.liveEntryFailed),
+        buttons: []
+      });
+      return false;
+    }
+    return true;
   }
 
   /*public onClickSaveBtn(): void {
--- Live-dashboard/src/environments/environment.ts
+++ Live-dashboard/src/environments/environment.ts
@@ -6,13 +6,20 @@
 export const environment = {
   production: false,
   "bootstrap": {
-    "service_url_extension": "/api_v3/index.php"
+    "service_url_extension": "/api_v3/index.php",
+    "error_message": "Invalid Application Parameters!"
   },
   "liveEntryService": {
     "streamStatusIntervalTimeInMs": 5000,
     "streamHealthIntervalTimeInMs": 10000
   },
   "externalLinks": {
-      "LEARN_MORE": "https://knowledge.kaltura.com/getting-started-kaltura-live"
+    "LEARN_MORE": "https://knowledge.kaltura.com/getting-started-kaltura-live"
+  },
+  "loadingError": {
+    "liveEntryFailed": "Failed to load Live Entry. Please restart application"
+  },
+  "flavorsDefinitions": {
+    "sourceFlavorId": "32"
   }
 };
